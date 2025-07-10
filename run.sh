121155248839970647f50ab8ed2a4de8b2677b37
Yyax13 <Yyax13@proton.me>
2025-07-11 00:05:54 +0200

    [CHORE] Updated run.sh to increase speed and optmize for using as a service

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
+nvm use
 while true; do
-    NODE_OPTIONS=--openssl-legacy-provider node .
-    echo "Bot caiu... reiniciando em 3 segundos"
-    sleep 1
-    clear
     echo "instalando dependencias"
     npm i
     clear
     echo "Bot iniciado"
+    NODE_OPTIONS=--openssl-legacy-provider node .
+    echo "Bot caiu... reiniciando em 3 segundos"
+    sleep 1
+    clear
 done
